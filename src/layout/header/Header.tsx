66afcbc9029d1e34d3f3f63ed8e7141514291019
IzmailovWeb <izmailovweb@gmail.com>
2024-01-21 17:05:20 +0300

    first commit

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import styled from "styled-components";
-import {Logo} from "../../../components/logo/Logo";
-import {Menu} from "../../../components/menu/Menu";
+import {Logo} from "../../components/logo/Logo";
+import {Container} from "../../components/Container";
+import {FlexWrapper} from "../../components/FlexWrapper";
+import {HeaderMenu} from "./headerMenu/HeaderMenu";
 
+const items = ["Home","Skills","Contact"]
 export const Header = () => {
     return (
         <StylledHeader>
-           <Logo/>
-            <Menu/>
+            <Container>
+                <FlexWrapper justiify={"space-between"} align={"center"}>
+                    <Logo/>
+                    <HeaderMenu menuItems={items}/>
+                </FlexWrapper>
+            </Container>
         </StylledHeader>
     );
 };
+
+
+
 const StylledHeader = styled.header`
-    background-color: coral;
-`;
+  background-color:rgba(31, 31, 32, 0.90);
+  padding: 20px 0px ;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 99999;
+`
 
